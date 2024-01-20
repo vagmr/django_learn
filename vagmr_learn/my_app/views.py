e292e4176f677d73969accd6555bb90f313c172b
vagmr <2737165206@qq.com>
2024-01-20 23:03:59 +0800

    init

---
+++ vagmr_learn/my_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(req):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+# Create your views here.
---
+++ vagmr_learn/vagmr_learn/urls.py
@@ -0,0 +1,20 @@
+"""
+URL配置为vagmr_learn项目。
+urlpatterns列表将URL路由到视图。有关更多信息，请参见：https://docs.djangoproject.com/en/4.2/topics/http/urls/
+示例：
+函数视图
+1. 添加导入：从my_app导入视图
+2. 将URL添加到urlpatterns：path('', views.home, name='home')
+基于类的视图
+1. 添加导入：从other_app.views导入Home
+2. 将URL添加到urlpatterns：path('', Home.as_view(), name='home')
+包括另一个URLconf
+1. 导入include()函数：从django.urls导入include、path
+2. 将URL添加到urlpatterns：path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
